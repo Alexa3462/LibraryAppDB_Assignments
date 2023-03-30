631890cc7e3f41c55319c4b8b806783560af8a9c
ligostaevaa <ligostaevaa17@gmail.com>
2023-03-30 14:58:41 -0500

    EL StepDefs has been created

---
+++ src/test/java/com/library/steps/B28G31_145_StepDefs_EL.java
@@ -0,0 +1,23 @@
+package com.library.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class B28G31_145_StepDefs_EL {
+
+
+    @Given("the {string} is on the home page")
+    public void the_is_on_the_home_page(String string) {
+
+    }
+    @When("the librarian gets borrowed books number")
+    public void the_librarian_gets_borrowed_books_number() {
+
+    }
+    @Then("borrowed books number information must match with DB")
+    public void borrowed_books_number_information_must_match_with_db() {
+
+    }
+
+}
